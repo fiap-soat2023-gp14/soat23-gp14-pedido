1d345ac31de32571768c9e0b2cfca1e25a174829
Silverton dos Santos Gimenes <rm349968@fiap.com.br>
2023-06-19 08:40:44 -0300

    adding exception handling

---
+++ fiap-application/src/infrastructure/exceptions/httpExceptionFilter.ts
@@ -0,0 +1,28 @@
+import { ExceptionFilter, Catch, ArgumentsHost, Logger } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export default class HttpExceptionFilter implements ExceptionFilter<Error> {
+  catch(exception: Error, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+
+    const { message, status } = this.isBusinessException(exception);
+    response.status(status).json({
+      message,
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+    });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public isBusinessException(exception: Error): any {
+    Logger.log(exception.stack);
+    return {
+      message: 'unknown',
+      status: 500,
+    };
+  }
+}
--- fiap-application/src/main.ts
+++ fiap-application/src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { Transport } from '@nestjs/microservices';
 import { AppModule } from "./app.module";
+import HttpExceptionFilter from './infrastructure/exceptions/httpExceptionFilter';
 
 
 async function bootstrap() {
   const port = process.env.PORT ? Number(process.env.PORT) : 8080;
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(port);
   console.log('Microservice listening on port:', port);
 }
