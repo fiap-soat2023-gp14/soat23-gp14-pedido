a6ce437a4d67221455de99c96593a2db4a6be672
Silverton <silverton.gimenes@ab-inbev.com>
2023-09-01 19:17:34 -0300

    migração da validação para UseCase

--- fiap-application/src/core/application/application.module.ts
+++ fiap-application/src/core/application/application.module.ts
@@ -1,42 +1,10 @@
 import { Module } from '@nestjs/common';
-import UserGateway from 'src/infrastructure/adapters/gateway/UserGateway';
-import MongoConnection from '../../infrastructure/MongoConnection';
-import PaymentGateway from '../../infrastructure/adapters/external/PaymentGateway';
-import OrderGateway from '../../infrastructure/adapters/gateway/OrderGateway';
-import ProductGateway from '../../infrastructure/adapters/gateway/ProductGateway';
 import DomainModule from '../domain/domain.module';
-import OrderUseCase from './usecase/OrderUseCase';
-import ProductUseCase from './usecase/ProductUseCase';
-import UserUseCase from './usecase/UserUseCase';
 
 @Module({
   imports: [DomainModule],
   controllers: [],
-  providers: [
-    ProductUseCase,
-    {
-      provide: 'IProductRepository',
-      useClass: ProductGateway,
-    },
-    OrderUseCase,
-    {
-      provide: 'IOrderRepository',
-      useClass: OrderGateway,
-    },
-    {
-      provide: 'IPaymentGateway',
-      useClass: PaymentGateway,
-    },
-    UserUseCase,
-    {
-      provide: 'IUserRepository',
-      useClass: UserGateway,
-    },
-    {
-      provide: 'IMongoDBAdapter',
-      useClass: MongoConnection,
-    },
-  ],
-  exports: [ProductUseCase, OrderUseCase, UserUseCase],
+  providers: [],
+  exports: [],
 })
 export default class ApplicationModule {}
