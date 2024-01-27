84e9043de7e2131a30e7594a3da842a6ffea6f84
Silverton Gimenes <silverton.gimenes@ab-inbev.com>
2024-01-27 17:03:25 -0300

    update strucutre

--- fiap-application/src/infrastructure/mocks/OrderMock.ts
+++ fiap-application/src/infrastructure/mocks/OrderMock.ts
@@ -5,6 +5,7 @@ import { CPF } from '../../core/domain/valueObjects/Cpf';
 import { Money } from '../../core/domain/valueObjects/Money';
 import { OrderEntity } from '../adapters/gateway/entity/OrderEntity';
 import { OrderEntityStatus } from '../adapters/gateway/enums/OrderEntityStatus';
+import { OrderResponseDTO } from '../../core/application/dto/OrderResponseDTO';
 
 export class OrderMock {
   public static async getOrder(): Promise<Order> {
@@ -14,7 +15,7 @@ export class OrderMock {
         id: '42',
         email: 'test@test.com',
         name: 'test untario',
-        cpf: await CPF.create('123456789'),
+        cpf: await CPF.create('12345678910'),
         phone: '123456789',
         createdAt: new Date('2024-01-26T17:41:00Z'),
         updatedAt: new Date('2024-01-26T17:41:00Z'),
@@ -50,7 +51,7 @@ export class OrderMock {
         _id: '42',
         email: 'test@test.com',
         name: 'test untario',
-        cpf: '123456789',
+        cpf: '12345678910',
         phone: '123456789',
         createdAt: new Date('2024-01-26T17:41:00Z'),
         updatedAt: new Date('2024-01-26T17:41:00Z'),
@@ -77,4 +78,59 @@ export class OrderMock {
       total: 100,
     };
   }
+
+  public static getOrderDTO(): OrderResponseDTO {
+    const expectedOrderResponse = {
+      id: '1',
+      customer: {
+        id: '42',
+        email: 'test@test.com',
+        name: 'test untario',
+        cpf: '12345678910',
+        phone: '123456789',
+      },
+      createdAt: new Date('2024-01-26T17:41:00Z'),
+      deliveredAt: new Date('2024-01-26T17:41:00Z'),
+      extraItems: undefined,
+      items: [
+        {
+          product: {
+            id: '3',
+            name: 'test product',
+            description: 'only unit test',
+            imageUrl: 'http://imagetest.com.png',
+            price: 100,
+            category: ProductCategory.SANDWICH,
+            createdAt: new Date('2024-01-26T17:41:00Z'),
+          },
+          observation: undefined,
+          quantity: 1,
+        },
+      ],
+      status: OrderStatus.RECEIVED,
+      total: 100,
+    };
+    return expectedOrderResponse;
+  }
+
+  public static getOrderBody() {
+    return {
+      customerId: '123abc',
+      items: [
+        {
+          productId: 'prod456',
+          quantity: 2,
+          observation: 'This is a test observation',
+        },
+        {
+          productId: 'prod789',
+          quantity: 1,
+          observation: 'Another test observation',
+        },
+      ],
+      extraItems: 'Extra item details',
+    };
+  }
+
+
 }
